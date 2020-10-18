f8e5a034a6c46d823e4c5c192bf2d0a55378e856
TerryThreatt <terry.threatt@gmail.com>
2020-10-18 18:56:55 -0500

    added homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/homepage'
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Homepage />
     </div>
   );
 }
