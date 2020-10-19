d2f8b483df56bb08679d98e358bc7eb008a15296
TerryThreatt <terry.threatt@gmail.com>
2020-10-18 19:08:30 -0500

    added sass to homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Homepage from './components/homepage'
+import './Homepage.styles.scss'
+import Homepage from './Homepage'
 
 function App() {
   return (
