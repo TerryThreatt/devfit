2251033490e8acc128b5c712f8d10f7cee3c59c0
TerryThreatt <terry.threatt@gmail.com>
2021-01-24 12:40:49 -0600

    add form input css

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 const FormInput = ({handleChange, label, ...otherProps}) => (
     <div className="group">
         <input type="text" className="form-input" onChange={handleChange} {...otherProps}/>
+        {
+            label ? (
+            <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                {label}
+            </label>
+            )
+            :
+            null
+        }
     </div>
 )
 
