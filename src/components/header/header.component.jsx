1e473f874d566e77d1481da467b7921711db2746
TerryThreatt <terry.threatt@gmail.com>
2021-01-24 11:28:43 -0600

    added signin to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,6 +19,9 @@ const Header = () => (
             <Link className="option" to="/contact">
                 CONTACT
             </Link>
+            <Link className="option" to="/signin">
+                SIGN IN
+            </Link>
         </div>
     </div>
 )
