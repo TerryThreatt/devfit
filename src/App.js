81e69b145a0cf637c3888d00d57a30b29d1ccd07
TerryThreatt <terry.threatt@gmail.com>
2021-01-30 18:09:06 -0600

    convert app.js to class comp

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import Homepage from './pages/homepage/Homepage.component'
 import ShopPage from './pages/shop/Shop.component'
 import Header from './components/header/header.component'
 import SignUpAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path="/shop"  component={ShopPage} />
-        <Route path="/signin"  component={SignUpAndSignOut} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null 
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path="/shop"  component={ShopPage} />
+          <Route path="/signin"  component={SignUpAndSignOut} />
+        </Switch>
+      </div>
+    )
+    }
 }
 
 export default App;
